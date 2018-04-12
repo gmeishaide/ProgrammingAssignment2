52026bf34fd8e48913462a7f9f9efbbfccb60c3e
Gabrail C. Meishaide <gabrail.meishaide@meishaide-consulting.com>
2018-04-12 18:18:33 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
+## Create cache inverse matrix by initializing the vector plots for variables c, d, and i by
+## setting up the inverse matrix for cache using the setinverse function and then creating
+## the function to pull vector data list  from the cache is called getinverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(c = matrix()) {
+  i <- NULL
+  set <- function(d) {
+    c <<- d
+    i <<- NULL
+  }
+  get <- function() c
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
-
 ## Write a short comment describing this function
+## The cacheSolve function reads the data from the
+## getinverse function once the data has been retrieved
+## from cache matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(c, ...) {
+  ## Return a matrix that is the inverse of 'c'
+  i <- c$getinverse()
+  if (!is.null(i)) {
+    message("retreiving cached data")
+    return(i)
+  }
+  data <- c$get()
+  i <- solve(data, ...)
+  c$setinverse(i)
+  i
 }
